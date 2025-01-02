0960a4a7275c488ffe67bd4cdc982b6485d7b9de
TaiseiIto <taisei1212@outlook.jp>
2025-01-02 13:54:54 +0900

    Build MikanOS #1

--- delete.sh
+++ delete.sh
@@ -2,6 +2,15 @@
 
 image=$1
 container=$2
+target=$3
+mount=$4
+
+# Delete target.
+rm -f $target
+
+# Delete mount.
+if mountpoint -q $mount; then sudo umount -l $mount; fi
+rm -rf $mount
 
 # Stop the docker container.
 if [ -n "$(docker ps --format {{.Names}} | grep -x $container)" ]; then
